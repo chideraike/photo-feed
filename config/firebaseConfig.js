ecb3011afb4833c6e1b24b6ea0ed6a29b2d31ea3
Chidera <42821245+chideraike@users.noreply.github.com>
2020-10-21 23:03:19 +0100

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApv8isjLMDb3Gl9u9VgrOB6L8RN4HanNw",
-    authDomain: "photo-feed-62a0b.firebaseapp.com",
-    databaseURL: "https://photo-feed-62a0b.firebaseio.com",
-    projectId: "photo-feed-62a0b",
-    storageBucket: "photo-feed-62a0b.appspot.com",
-    messagingSenderId: "998250116264",
-    appId: "1:998250116264:web:c07f7af82bb1677ae3be9d"
+    // Your Firebase Config Details
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
